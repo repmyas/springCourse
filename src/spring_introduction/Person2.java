383351f85f1c6c9cd8aeaa975d93583d28512d9b
Alex <m9lndev@gmail.com>
2021-07-25 11:55:00 +0700

    value on xml

--- src/spring_introduction/Person2.java
+++ src/spring_introduction/Person2.java
@@ -2,6 +2,17 @@ package spring_introduction;
 
 public class Person2 {
     private Pet pet;
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    private int age;
 
 
     public void setPet(Pet pet) {
@@ -11,5 +22,6 @@ public class Person2 {
     public void say(){
         this.pet.say();
         System.out.println("Person2 say");
+        System.out.println(name + ' ' + age);
     }
 }
