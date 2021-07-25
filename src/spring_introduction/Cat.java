502226deeb4ed1b7d7a55db09869278c7257e8b4
Alex <m9lndev@gmail.com>
2021-07-25 12:40:34 +0700

    learn scope bean

--- src/spring_introduction/Cat.java
+++ src/spring_introduction/Cat.java
@@ -2,6 +2,16 @@ package spring_introduction;
 
 public class Cat implements Pet {
 
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
     @Override
     public void say(){
         System.out.println("Cat is say ");
